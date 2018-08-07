ef6f196b0f76cd3b3df3f58c1437047341ac7d9a
thanghuyhoangth <thanghuyhoanghst@gmail.com>
2018-08-07 23:12:02 +0700

    create dagger 2

---
+++ app/src/main/java/hmusic/music/hoang/com/thhmusic/MainApplication.java
@@ -0,0 +1,22 @@
+package hmusic.music.hoang.com.thhmusic;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
+
+import hmusic.music.hoang.com.thhmusic.data.source.repository.RepositoryModule;
+
+public class MainApplication extends Application {
+    private AppComponent mAppComponent;
+
+    public AppComponent getAppComponent() {
+        if (mAppComponent == null) {
+            mAppComponent = DaggerAppComponent.builder()
+                    .applicationModule(new ApplicationModule(getApplicationContext()))
+
+                    .repositoryModule(new RepositoryModule())
+                    .build();
+        }
+        return mAppComponent;
+    }
+
+}
---
+++ app/src/main/java/hmusic/music/hoang/com/thhmusic/utils/dagger/ApplicationContext.java
@@ -0,0 +1,7 @@
+package hmusic.music.hoang.com.thhmusic.utils.dagger;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+public @interface ApplicationContext {
+}
